c653d5fbcd02f67f71abb91f7df0496a15fb51b3
Mega06116 <164973143+Mega06116@users.noreply.github.com>
2024-05-16 19:20:41 +0800

    Add files via upload

---
+++ LoRA/export_model.py
@@ -0,0 +1,13 @@
+from llmtuner import export_model
+
+model_args="D:/wzh/models"
+finetuning_arg= "D:\wzh\train\train_2024-03-04-20-52-23",
+model_name_or_path="D:/wzh/models"
+
+
+def main():
+    export_model()
+
+
+if __name__ == "__main__":
+    main()
